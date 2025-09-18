92aea53476d21f4b6ebcdb358e9d60bcddae4efe
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2025-09-18 16:58:33 +0400

    Add react-router with Home, Login, Register pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,30 @@
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import Header from "./layout/Header";
 
+const Layout = () => (
+  <>
+    <Header />
+    <Outlet />
+  </>
+);
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      { path: "", element: <Home /> },
+      { path: "login", element: <Login /> },
+      { path: "register", element: <Register /> },
+    ],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <Header />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
