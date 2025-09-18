6d85d500097140dd9fdde371cd06dfedac9dc33e
Luka Badali <150358418+Bdmnss@users.noreply.github.com>
2025-09-18 17:58:13 +0400

    Update dynamic login/register button in Header based on route

--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -1,13 +1,23 @@
+import { Link, useLocation } from "react-router-dom";
 import Icon from "../icons/Icon";
 
 const Header = () => {
+  const location = useLocation();
+  const isLogin = location.pathname === "/login";
   return (
     <header className="flex items-center justify-between px-24 py-7">
       <Icon type="LogoIcon" />
-      <div className="flex items-center gap-2">
-        <Icon type="UserIcon" />
-        <p className="text-primaryText text-xs">Log in</p>
-      </div>
+      {isLogin ? (
+        <Link to="/register" className="flex items-center gap-2">
+          <Icon type="UserIcon" />
+          <p className="text-primaryText text-xs">Register</p>
+        </Link>
+      ) : (
+        <Link to="/login" className="flex items-center gap-2">
+          <Icon type="UserIcon" />
+          <p className="text-primaryText text-xs">Log in</p>
+        </Link>
+      )}
     </header>
   );
 };
